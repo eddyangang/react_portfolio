85ccb576793ed41c2302f3cdeb0e3765fe272399
eddyangang <eddyyang@berkeley.edu>
2020-08-20 15:05:13 -0700

    changed static path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("Client/build"));
 }
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
